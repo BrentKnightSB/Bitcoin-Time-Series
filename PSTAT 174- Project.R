69faeb3085d7192cb881ff87f2aa79945b8fb5e9
BrentKnightSB <36978703+BrentKnightSB@users.noreply.github.com>
2018-03-01 20:57:00 -0800

    Add files via upload

--- PSTAT 174- Project.R
+++ PSTAT 174- Project.R
@@ -17,10 +17,21 @@ crypto <- newdata[myvars]
 crypto$date <- as.Date(crypto$date)
 
 #Cutting off dates before 4/1/17
-newcrypto <- subset(crypto, date > "2017-04-01")
+newcrypto <- subset(crypto, date > "2017-03-29")
 
 #Plotting
 ts.plot(data=newcrypto)
+crypto2 = NULL
 
 
+
+crypto3 <- newcrypto["close"]
+n <- 7
+cryptoFinal <- aggregate(newcrypto, list(rep(1:(nrow(newcrypto)%/%n+1),each=n, len=nrow(newcrypto))),mean)[-1]
+
+cryptoFinal$Week <- seq.int(nrow(cryptoFinal))
+
+myvars2 <- c("Week","close")
+cryptoFinal2 <- cryptoFinal[myvars2]
+ts.plot(data=cryptoFinal2)
 
